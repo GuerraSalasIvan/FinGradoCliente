f55b5a0009ecc4f20750764fa9d989b6b0b5b301
GuerraSalasIvan <ivan.guerra.salas.al@iespoligonosur.org>
2024-01-16 09:50:12 +0100

    copiado

---
+++ app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Usuarios)
+admin.site.register(Categoria_Persona)
+admin.site.register(Deportes)
+admin.site.register(Rel_Usu_Equi)
+admin.site.register(Votacion)
+admin.site.register(CuentaBancaria)
+admin.site.register(Promocion)
